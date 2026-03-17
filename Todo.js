49ba2f22a3054d6bb1b9266c6aaffb0e4c255593
alisinaqurbani2010 <alisinaqurbani2010@gamil.com>
2026-03-17 02:37:57 -0700

    todo list homeWork

---
+++ Todo.js
@@ -0,0 +1,46 @@
+const menu = document.getElementById("menu");
+const menuDiv = document.getElementById("menuDiv")
+const add = document.getElementById("add");
+const saved = document.getElementById("saved");
+const form = document.getElementById("form")
+let input = document.getElementById("input")
+const div = document.getElementById("list")
+const body = document.getElementById("body")
+const save = document.getElementById("save")
+let result  = "";
+let isOpen = false;
+let isSee = true;
+menu.addEventListener("click",()=>{
+    if(isOpen === false){
+        menuDiv.style.marginTop="5px";
+        menuDiv.style.transition="1.3s"
+        menu.textContent ="Close"
+        isOpen = true
+    }
+    else{
+        menuDiv.style.marginTop="-200px";
+        menu.textContent ="Menu"
+        isOpen = false;
+    }
+    
+});
+add.addEventListener("click",()=>{
+    form.style.opacity="1";
+    isSee = false;
+    if(isSee === true){
+        div.style.opacity="0"
+    }
+})
+save.addEventListener("click",()=>{
+    result += input.value;
+    input.value =""
+});
+saved.addEventListener("click",()=>{
+    form.style.opacity="0"
+    if(isSee === false){
+        let h1 = document.createElement("h1");
+    h1.textContent=result;
+    div.append(h1);
+    }
+    isSee === true
+})
